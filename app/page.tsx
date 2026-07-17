9c0845b4f7ea319fda3b9270798f24c858b06aba
iehf <iehf@ukr.net>
2026-07-17 10:06:25 +0300

    fix for auto deploy on App Hosting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ProductList from "@/components/Products/ProductList";
 import AddProductButton from "@/components/Products/AddProductButton";
 import { productService } from "@/lib/services/productService";
 
-export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const products = await productService.getProducts();
