a37cab52c5f5d868984162db0a27108da944bb76
iehf <iehf@ukr.net>
2026-07-17 10:37:24 +0300

    replace /n back

--- lib/firebase/firebaseAdmin.ts
+++ lib/firebase/firebaseAdmin.ts
@@ -14,7 +14,7 @@ export function getFirebaseAdminApp(): App {
     credential: cert({
       projectId: process.env.APP_FIREBASE_PROJECT_ID,
       clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.APP_FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
     storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET,
   });
