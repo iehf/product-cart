764290e85aa61c34eb561217d8207789ed1dd3bb
iehf <iehf@ukr.net>
2026-07-17 09:21:51 +0300

    Env for App Hosting

--- lib/firebase/firebaseAdmin.ts
+++ lib/firebase/firebaseAdmin.ts
@@ -1,14 +1,18 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 
-const firebaseAdminApp = getApps().length === 0
-  ? initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    })
-  : getApps()[0];
+const firebaseAdminApp =
+  getApps().length === 0
+    ? initializeApp({
+        credential: cert({
+          projectId: process.env.APP_FIREBASE_PROJECT_ID,
+          clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(
+            /\\n/g,
+            "\n",
+          ),
+        }),
+        storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET,
+      })
+    : getApps()[0];
 
 export default firebaseAdminApp;
