5406bf4247193e980bc1cf1c094215a0723ebff7
iehf <iehf@ukr.net>
2026-07-21 12:36:33 +0300

    firebase credential fix

--- lib/firebase/firebaseAdmin.ts
+++ lib/firebase/firebaseAdmin.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp, cert, App } from "firebase-admin/app";
+import { initializeApp, getApps, getApp, App } from "firebase-admin/app";
 
 let app: App | null = null;
 
@@ -11,11 +11,6 @@ export function getFirebaseAdminApp(): App {
   }
 
   app = initializeApp({
-    credential: cert({
-      projectId: process.env.APP_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.APP_FIREBASE_PRIVATE_KEY,
-    }),
     storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET,
   });
 
